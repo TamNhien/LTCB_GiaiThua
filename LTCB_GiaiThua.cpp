3bf47a9f351cd6c4290af056ad8f66e1f727292e
TamNhien <canhthien91@gmail.com>
2024-03-04 12:22:19 +0700

    Add project files.

---
+++ LTCB_GiaiThua.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int i = 1, sonhap, giaithua = 1;
+	cout << "Nhap so can tinh giai thua : "; cin >> sonhap;
+	while (i <= sonhap) {
+		giaithua *= i;
+		i++;
+	}
+	cout << "Giai thua cua" << sonhap << " la : " << giaithua << endl;
+	return 0;
+}
